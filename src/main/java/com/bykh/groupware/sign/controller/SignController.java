67a34b31e7491074fc352084b0720d07a8221e2d
su-pq <cctvmaxmax@gamil.com>
2023-05-17 17:22:17 +0900

    add ex)doc type -gian and js

--- src/main/java/com/bykh/groupware/sign/controller/SignController.java
+++ src/main/java/com/bykh/groupware/sign/controller/SignController.java
@@ -1,4 +1,4 @@
-package com.bykh.groupware.sign;
+package com.bykh.groupware.sign.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
